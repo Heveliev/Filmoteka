cba07b884d297a13159e748dc6dc6abb20663eca
Heveliev-destress <tolik05.03@ukr.net>
2023-01-22 22:53:05 +0200

    add function getapi, num pag, and library

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+document.addEventListener('DOMContentLoaded',()=>{Notify.info('This product uses the TMDB API but is not endorsed or certified by TMDB.',{useIcon: false})})
