f6f5a178d485cbe3097a65ec8d0c8783322cc780
Oleksandr Zahorskyi <alex.alzag.zagorsky@gmail.com>
2023-01-27 18:24:41 +0200

    Update trending page

---
+++ src/js/GETAPI/setMoviesGenres.js
@@ -0,0 +1,17 @@
+export async function setMoviesGenres() {
+  const GENRES_KEY = 'saved-genres';
+  let genres = {};
+
+  if (localStorage.getItem(GENRES_KEY)) {
+    return;
+  } else {
+    const response = await fetchMoviesGenres();
+
+    for (const genre of response.genres) {
+      const { id, name } = genre;
+      genres[id] = name;
+    }
+
+    localStorage.setItem(GENRES_KEY, JSON.stringify(genres));
+  }
+}
--- src/js/renderTrendigMovies/renderTrendingPage.js
+++ src/js/renderTrendigMovies/renderTrendingPage.js
@@ -2,61 +2,22 @@ import {
   fetchMoviesGenres,
   fetchTrendingMoviesInfo,
 } from '../GETAPI/fetchTrendingMoviesInfo';
-import { renderMovieDetalis } from './renderMovieDetails';
-import { refs } from '../refs/refs';
+import { renderMoviesCards } from '../createMoviesMarkup/renderMoviesCards';
+import { setMoviesGenres } from '../GETAPI/setMoviesGenres';
 
-
-const GENRES_KEY = 'saved-genres';
 const MOVIES_KEY = 'saved-movies';
 
-let genres = {};
 let currentPage = 1;
 
-if (localStorage.getItem(GENRES_KEY)) {
-  const savedGenres = localStorage.getItem(GENRES_KEY);
-
-  genres = JSON.parse(savedGenres);
-} else {
-  setMoviesGenres();
-}
+setMoviesGenres();
 
 async function renderTrendingPage() {
   const response = await fetchTrendingMoviesInfo(currentPage);
   const moviesObj = response.results;
 
-  refs.moviesList.innerHTML = moviesObj
-    .map(
-      movie => `<li class="films__card">
-        <a href="" class="films__link">
-          <img <img src="https://image.tmdb.org/t/p/w500/${
-            movie.poster_path
-          }"  alt="film poster" class="films__picture" />
-          <p class="films__title">${movie.title}</p>
-          <div class="films__details">
-            <p class="films__genres film-font-style">${renderMovieDetalis(
-              movie,
-              genres
-            )}</p>
-            <span class="films__rate">${movie.vote_average.toFixed(1)}</span>
-          </div>
-        </a>
-      </li>`
-    )
-    .join('');
+  renderMoviesCards(moviesObj);
 
   localStorage.setItem(MOVIES_KEY, JSON.stringify(moviesObj));
 }
-
-// function ganres
-async function setMoviesGenres() {
-  const response = await fetchMoviesGenres();
-
-  for (const genre of response.genres) {
-    const { id, name } = genre;
-    genres[id] = name;
-  }
-
-  localStorage.setItem(GENRES_KEY, JSON.stringify(genres));
-};
 
 renderTrendingPage();
