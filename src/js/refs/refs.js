b7e9dcb282046135abfa2f282c4d60d89076dccf
Yulia Kutova <kutova85@gmail.com>
2023-01-29 13:30:34 +0200

    add pagination

--- src/js/refs/refs.js
+++ src/js/refs/refs.js
@@ -3,4 +3,5 @@ export { refs };
 const refs = {
   // searchInput: document.querySelector(input),
   moviesList: document.querySelector('.films__list'),
+  paginationBox: document.querySelector('.page-number__list'),
 };
