b1741a367dcd161802e66a8ffbe78c6658614f6f
Andrii Shubin <shubinanri@gmail.com>
2023-01-29 18:52:10 +0200

    add new code

--- src/js/createMoviesMarkup/renderMoviesCards.js
+++ src/js/createMoviesMarkup/renderMoviesCards.js
@@ -3,10 +3,14 @@ import { refs } from '../refs/refs';
 export function renderMoviesCards(moviesObjects) {
   return (refs.moviesList.innerHTML = moviesObjects
     .map(
-      movie => `<li class="films__card">
-        <a href="" class="films__link">
+      movie => `<li class="films__card" data-id=${movie.id}> <div class="films__link">
         <div class="films__overflow-wrapper"><img src="https://image.tmdb.org/t/p/w500/${movie.poster_path
-        }"  alt="film poster" class="films__picture" /></div>
+        }"  alt="film poster" class="films__picture" />
+        <div class="films__overlay">
+        <p class="films__trailer-text">Watch trailer</p>
+        <img src="" alt="" class="films__trailer-icon">
+      </div>
+        </div>
           <p class="films__title">${movie.title}</p>
           <div class="films__details">
             <p class="films__genres film-font-style">${createMovieDetalisMarkup(
@@ -14,13 +18,15 @@ export function renderMoviesCards(moviesObjects) {
         )}</p>
             <span class="films__rate">${movie.vote_average.toFixed(1)}</span>
           </div>
-        </a>
+        </div>
       </li>`
     )
     .join(''));
 }
 
 export function createMovieDetalisMarkup(movie) {
+  // console.log(movie.id);
+
   const savedGenres = localStorage.getItem('saved-genres');
   const genres = JSON.parse(savedGenres);
 
---
+++ src/js/createTrailer/createTrailer.js
@@ -0,0 +1,50 @@
+import * as basicLightbox from 'basiclightbox';
+import axios from 'axios';
+import Notiflix from "notiflix";
+
+
+
+const getRefs = () => {
+    return {
+        galleryItemsOverlay: document.querySelector('.films__list'),
+    }
+}
+const API_KEY = "7752b7319b478a62fea227c419e04b15";
+const API_URL = "https://api.themoviedb.org/3/";
+
+const refs = getRefs();
+axios.defaults.baseURL = API_URL;
+
+refs.galleryItemsOverlay.addEventListener('click', onGalleryItemsClick);
+
+function onGalleryItemsClick(e) {
+    // console.log(e.target.className);
+    if (e.target.className !== 'films__overlay' && e.target.className !== 'films__trailer-text') {
+        console.log("Click Error!");
+        return
+    }
+    else {
+        e.preventDefault;
+        const cardId = (e.target.parentNode.parentNode.parentNode.parentNode.dataset.id);
+        openVideoTrailer(cardId);
+    }
+}
+
+function openVideoTrailer(id) {
+    const url = `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${API_KEY}`;//&language=en-US
+    // Notiflix.Loading.pulse();
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data.results[0].key + " : Youtube key");
+            const id = data.results[0].key;
+            // const instance = basicLightbox.create(`<iframe src="https://www.youtube.com/embed/${id}" width="560" height="315" frameborder="0"></iframe>`);
+
+            const instance = basicLightbox.create(`<iframe width="80%" height="80%" src='https://www.youtube.com/embed/${id}'frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`);
+            console.log("instance");
+            instance.show();
+        })
+        .catch(() => {
+            console.log("Catch Error!");;
+        });
+}
