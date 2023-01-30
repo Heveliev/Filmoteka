38260eefa6d320b4e10255e59d5f3e979124ed66
Oleksandr Zahorskyi <alex.alzag.zagorsky@gmail.com>
2023-01-30 23:06:59 +0200

    bugs fix

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import './js/renderTrendigMovies/renderTrendingPage';
+import './js/modalFilm/renderModalFilm';
 import './js/filmsByName/fetchFilmsByName';
--- src/library.js
+++ src/library.js
@@ -1,2 +1,2 @@
-
 import './js/renderQueueWatchedFilms/renderQueueWachedFilms';
+import './js/modalFilm/renderModalFilm';
