5e93977a36236fa4d4ae83617855c83b3c918396
Heveliev-destress <tolik05.03@ukr.net>
2023-01-31 13:05:54 +0200

    fix merge

--- src/js/filmsByName/markup.js
+++ src/js/filmsByName/markup.js
@@ -1,13 +1,8 @@
 function createMarkup(arr = []) {
-    return arr.reduce((acc, { id, poster_path, title, genre_ids, release_date, vote_average }) => acc +
-        `<li class="films__card" data-id=${id}>
+    return arr.reduce((acc, {id, poster_path, title, genre_ids, release_date, vote_average}) => acc + `<li class="films__card" 
+    data-id = ${id}>
         <a href="" class="films__link">
-        <div class="films__overflow-wrapper"><img src="https://image.tmdb.org/t/p/w500/${poster_path}" alt="film poster" class="films__picture" />
-        <div class="films__overlay">
-        <p class="films__trailer-text">&#10005 Watch trailer</p>
-        <img src="" alt="" class="films__trailer-icon">
-      </div>
-        </div>    
+        <img src="https://image.tmdb.org/t/p/w500/${poster_path}" alt="film poster" class="films__picture" />   
             <p class="films__title">${title}</p>
             <div class="films__details">
                 <p class="films__genres film-font-style">${genre_ids}</p>
