a90e646269987f9075cb7456cdc5c9256332ab04
Heveliev-destress <tolik05.03@ukr.net>
2023-01-31 21:07:18 +0200

    fix

--- src/js/filmsByName/fetchFilmsByName.js
+++ src/js/filmsByName/fetchFilmsByName.js
@@ -12,7 +12,52 @@ const refs = {
 refs.form.addEventListener('submit', onSearchByName)
 let value;
 let globalCurrentPage = 0;
-
+function renderPagination(currentPage, allPages) {
+    let markup = '';
+    let beforeTwoPage = currentPage - 2;
+    let beforePage = currentPage - 1;
+    let afterPage = currentPage + 1;
+    let afterTwoPage = currentPage + 2;
+    globalCurrentPage = currentPage;
+  
+    if (currentPage > 1) {
+      markup += `<li class="page-number__item arrows">&#129144;</li>`;
+      markup += `<li class="page-number__item number">1</li>`;
+    }
+  
+    if (currentPage > 4) {
+      markup += `<li class="page-number__item">...</li>`;
+    }
+  
+    if (currentPage > 3) {
+      markup += `<li class="page-number__item number">${beforeTwoPage}</li>`;
+    }
+  
+    if (currentPage > 2) {
+      markup += `<li class="page-number__item number">${beforePage}</li>`;
+    }
+  
+    markup += `<li class="page-number__item current_page number"><b>${currentPage}</b></li>`;
+  
+    if (allPages - 1 > currentPage) {
+      markup += `<li class="page-number__item number">${afterPage}</li>`;
+    }
+  
+    if (allPages - 2 > currentPage) {
+      markup += `<li class="page-number__item number">${afterTwoPage}</li>`;
+    }
+  
+    if (allPages - 3 > currentPage) {
+      markup += `<li class="page-number__item">...</li>`;
+    }
+  
+    if (allPages > currentPage) {
+      markup += `<li class="page-number__item number">${allPages}</li>`;
+      markup += `<li class="page-number__item arrows">&#129146;</li>`;
+    }
+  
+    refs.paginationBox.innerHTML = markup;
+  }
 async function onSearchByName(e) {
     e.preventDefault();
     value = e.target.query.value.trim();
@@ -73,51 +118,6 @@ async function handlerTrendingPagination(evt) {
 }
 
 
-function renderPagination(currentPage, allPages) {
-    let markup = '';
-    let beforeTwoPage = currentPage - 2;
-    let beforePage = currentPage - 1;
-    let afterPage = currentPage + 1;
-    let afterTwoPage = currentPage + 2;
-    globalCurrentPage = currentPage;
-  
-    if (currentPage > 1) {
-      markup += `<li class="page-number__item arrows">&#129144;</li>`;
-      markup += `<li class="page-number__item number">1</li>`;
-    }
-  
-    if (currentPage > 4) {
-      markup += `<li class="page-number__item">...</li>`;
-    }
-  
-    if (currentPage > 3) {
-      markup += `<li class="page-number__item number">${beforeTwoPage}</li>`;
-    }
-  
-    if (currentPage > 2) {
-      markup += `<li class="page-number__item number">${beforePage}</li>`;
-    }
-  
-    markup += `<li class="page-number__item current_page number"><b>${currentPage}</b></li>`;
-  
-    if (allPages - 1 > currentPage) {
-      markup += `<li class="page-number__item number">${afterPage}</li>`;
-    }
-  
-    if (allPages - 2 > currentPage) {
-      markup += `<li class="page-number__item number">${afterTwoPage}</li>`;
-    }
-  
-    if (allPages - 3 > currentPage) {
-      markup += `<li class="page-number__item">...</li>`;
-    }
-  
-    if (allPages > currentPage) {
-      markup += `<li class="page-number__item number">${allPages}</li>`;
-      markup += `<li class="page-number__item arrows">&#129146;</li>`;
-    }
-  
-    refs.paginationBox.innerHTML = markup;
-  }
+
 
   export {onSearchByName};
