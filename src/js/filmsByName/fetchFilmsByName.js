0fcb512cfdfcd35ff9190f7f5d8e935ac6708dc7
Heveliev-destress <tolik05.03@ukr.net>
2023-01-31 21:22:10 +0200

    fix

--- src/js/filmsByName/fetchFilmsByName.js
+++ src/js/filmsByName/fetchFilmsByName.js
@@ -2,7 +2,7 @@ import { getData } from "../getapi/getData";
 import { renderMoviesCards } from "../createMoviesMarkup/renderMoviesCards";
 import { renderTrendingPage } from "../renderTrendigMovies/renderTrendingPage";
 import { saveMoviesToLoсalStorage} from "../common/common";
-import { renderPagination } from "../createNumbPage.js/numbPage";
+// import { renderPagination } from "../createNumbPage.js/numbPage";
 const refs = {
     form: document.querySelector('.header-form'),
     failureMassege: document.querySelector('.js-failure-massege'),
@@ -11,7 +11,7 @@ const refs = {
 
 refs.form.addEventListener('submit', onSearchByName)
 let value;
-// let globalCurrentPage = 0;
+let globalCurrentPage = 0;
 
 async function onSearchByName(e) {
     e.preventDefault();
@@ -39,52 +39,52 @@ async function onSearchByName(e) {
 
     localStorage.setItem('saved-movies', JSON.stringify(data.results));
 }
-// function renderPagination(currentPage, allPages) {
-//   let markup = '';
-//   let beforeTwoPage = currentPage - 2;
-//   let beforePage = currentPage - 1;
-//   let afterPage = currentPage + 1;
-//   let afterTwoPage = currentPage + 2;
-//   globalCurrentPage = currentPage;
+function renderPagination(currentPage, allPages) {
+  let markup = '';
+  let beforeTwoPage = currentPage - 2;
+  let beforePage = currentPage - 1;
+  let afterPage = currentPage + 1;
+  let afterTwoPage = currentPage + 2;
+  globalCurrentPage = currentPage;
 
-//   if (currentPage > 1) {
-//     markup += `<li class="page-number__item arrows">&#129144;</li>`;
-//     markup += `<li class="page-number__item number">1</li>`;
-//   }
+  if (currentPage > 1) {
+    markup += `<li class="page-number__item arrows">&#129144;</li>`;
+    markup += `<li class="page-number__item number">1</li>`;
+  }
 
-//   if (currentPage > 4) {
-//     markup += `<li class="page-number__item">...</li>`;
-//   }
+  if (currentPage > 4) {
+    markup += `<li class="page-number__item">...</li>`;
+  }
 
-//   if (currentPage > 3) {
-//     markup += `<li class="page-number__item number">${beforeTwoPage}</li>`;
-//   }
+  if (currentPage > 3) {
+    markup += `<li class="page-number__item number">${beforeTwoPage}</li>`;
+  }
 
-//   if (currentPage > 2) {
-//     markup += `<li class="page-number__item number">${beforePage}</li>`;
-//   }
+  if (currentPage > 2) {
+    markup += `<li class="page-number__item number">${beforePage}</li>`;
+  }
 
-//   markup += `<li class="page-number__item current_page number"><b>${currentPage}</b></li>`;
+  markup += `<li class="page-number__item current_page number"><b>${currentPage}</b></li>`;
 
-//   if (allPages - 1 > currentPage) {
-//     markup += `<li class="page-number__item number">${afterPage}</li>`;
-//   }
+  if (allPages - 1 > currentPage) {
+    markup += `<li class="page-number__item number">${afterPage}</li>`;
+  }
 
-//   if (allPages - 2 > currentPage) {
-//     markup += `<li class="page-number__item number">${afterTwoPage}</li>`;
-//   }
+  if (allPages - 2 > currentPage) {
+    markup += `<li class="page-number__item number">${afterTwoPage}</li>`;
+  }
 
-//   if (allPages - 3 > currentPage) {
-//     markup += `<li class="page-number__item">...</li>`;
-//   }
+  if (allPages - 3 > currentPage) {
+    markup += `<li class="page-number__item">...</li>`;
+  }
 
-//   if (allPages > currentPage) {
-//     markup += `<li class="page-number__item number">${allPages}</li>`;
-//     markup += `<li class="page-number__item arrows">&#129146;</li>`;
-//   }
+  if (allPages > currentPage) {
+    markup += `<li class="page-number__item number">${allPages}</li>`;
+    markup += `<li class="page-number__item arrows">&#129146;</li>`;
+  }
 
-//   refs.paginationBox.innerHTML = markup;
-// }
+  refs.paginationBox.innerHTML = markup;
+}
 
 async function handlerTrendingPagination(evt) {
     function renderNewMoviesPage(pageNum) {
