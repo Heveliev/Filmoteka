41c397838b026098dc32e7a7869f9239245df2c1
Sergey Opanasenko <svopanasenko22@gmail.com>
2023-03-01 19:06:35 +0100

    add modalSt

---
+++ src/js/modalTeam/modalTeam.js
@@ -0,0 +1,19 @@
+const studentsRef = document.querySelector('.footer-text');
+const teamDesk = document.querySelector('.');
+const modalStudentRef = document.querySelector('.modal_student');
+
+
+// const p1Ref = document.querySelector('#p1');
+// const p2Ref = document.querySelector('#p2');
+// const p3Ref = document.querySelector('#p3');
+// const p4Ref = document.querySelector('#p4');
+// const p5Ref = document.querySelector('#p5');
+// const p6Ref = document.querySelector('#p6');
+// const p7Ref = document.querySelector('#p7');
+// const p8Ref = document.querySelector('#p8');
+// const p9Ref = document.querySelector('#p9');
+// const p10Ref = document.querySelector('#p10');
+
+// function slider(e) {
+//     e.preventDefault();
+// };
