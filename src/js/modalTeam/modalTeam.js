19541c1e3c6aec199394ca14301d8dcddc595a08
Sergey Opanasenko <svopanasenko22@gmail.com>
2023-03-05 12:45:52 +0100

    add modal team

--- src/js/modalTeam/modalTeam.js
+++ src/js/modalTeam/modalTeam.js
@@ -1,19 +1,20 @@
-const studentsRef = document.querySelector('.footer-text');
-const teamDesk = document.querySelector('.');
-const modalStudentRef = document.querySelector('.modal_student');
+const closeBtnRef = document.querySelector('.close-button');
+const overlayRef = document.querySelector('.overlay');
+const footerTextRef = document.querySelector('a.footer-text');
 
 
-// const p1Ref = document.querySelector('#p1');
-// const p2Ref = document.querySelector('#p2');
-// const p3Ref = document.querySelector('#p3');
-// const p4Ref = document.querySelector('#p4');
-// const p5Ref = document.querySelector('#p5');
-// const p6Ref = document.querySelector('#p6');
-// const p7Ref = document.querySelector('#p7');
-// const p8Ref = document.querySelector('#p8');
-// const p9Ref = document.querySelector('#p9');
-// const p10Ref = document.querySelector('#p10');
+closeBtnRef.addEventListener('click', closeModal);
 
-// function slider(e) {
-//     e.preventDefault();
-// };
+function closeModal() {
+    overlayRef.classList.add("is-hidden");
+    document.body.classList.remove('no-scroll');
+};
+
+footerTextRef.addEventListener('click', openModal);
+console.log(footerTextRef);
+
+function openModal(e) {
+    e.preventDefault();
+    overlayRef.classList.remove('is-hidden');
+    document.body.classList.add('no-scroll');
+};
